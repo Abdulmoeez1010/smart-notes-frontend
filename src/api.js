7e278fd1ceabe147d0b93ce8d3eccff85fbcf6e2
Abdulmoeez1010 <moizabdul073@gmail.com>
2026-08-24 16:27:25 +0500

    Update the render url of backend in api.js

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE = "http://127.0.0.1:8000";
+const API_BASE = "https://smart-notes-rag-assistant.onrender.com";
 
 export const ingestYoutube = (videoUrl) =>
   axios.post(`${API_BASE}/ingest/youtube`, { video_url: videoUrl });
